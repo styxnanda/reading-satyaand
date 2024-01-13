ba83f31539604e472aed8daa946ecb5b24a7787a
Satya Ananda <satyaananda65@gmail.com>
2024-01-13 17:41:39 +0700

    Backend changes.

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,13 @@ const port = process.env.PORT || 5001;
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
 app.get("/pdf/:filename", (req, res) => {
 	res.sendFile(path.join(__dirname, "public", req.params.filename));
 });
 
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+	res.send("THIS IS MY WORLD!");
 });
 
 app.listen(port, () => {
